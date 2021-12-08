e08b2e4395d7f52cb2207be559f25889dadeea42
boldtrn <boldtrn@gmail.com>
2021-12-08 19:21:05 +0100

    Request location updates from the Android system

--- android/app/src/main/java/com/graphhopper/maps/MainActivity.java
+++ android/app/src/main/java/com/graphhopper/maps/MainActivity.java
@@ -1,5 +1,91 @@
 package com.graphhopper.maps;
 
-import com.getcapacitor.BridgeActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Message;
 
-public class MainActivity extends BridgeActivity {}
+import androidx.core.app.ActivityCompat;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Logger;
+
+public class MainActivity extends BridgeActivity {
+
+    LocationManager locationManager = null;
+
+    private final int REQUEST_LOCATION = 42;
+
+    private final LocationListener listener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            // A new location update is received.  Do something useful with it.  In this case,
+            // we're sending the update to a handler which then updates the UI with the new
+            // location.
+            Logger.debug("GH Android", "Received a location update " + location.toString());
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestLocationUpdates();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        requestLocationUpdates();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (locationManager != null) {
+            locationManager.removeUpdates(listener);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (locationManager != null) {
+            locationManager.removeUpdates(listener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_LOCATION) {
+            requestLocationUpdates();
+        }
+    }
+
+    private void requestLocationUpdates() {
+        // Request permission, if it is not yet provided
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Requires at least Marshmallow
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+            }
+            Logger.warn("GH Android", "No location permission available");
+            return;
+        }
+        Logger.warn("GH Android", "Start requesting Location Updates");
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                0,
+                0,
+                listener);
+    }
+
+}
