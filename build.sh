4eb2e32dfed00aa933dcfda85318e567328ef81a
Peter <graphhopper@gmx.de>
2023-02-07 12:26:51 +0100

    updated to use latest GH Maps (#25)
    * updated to use latest GH Maps
    
    * minor build fix
    
    * minor build fix
    
    * dep fixes
    
    * target 31 as required from capacitor https://github.com/ionic-team/capacitor/commit/05503cd13c5f08ad2353cbe9bb69d9e8e1c95f73
    
    * fix minor other problems
    
    * try minor revert
    
    * different foreground images
    
    * Remove unneeded Java code, as Capacitor can handle this internally now
    
    * Update Logo
    
    * Update CI build
    
    ---------
    
    Co-authored-by: boldtrn <boldtrn@gmail.com>

--- build.sh
+++ build.sh
@@ -3,19 +3,23 @@
 cp config.js graphhopper-maps/
 
 # Build GraphHopper Maps
-# TODO: sync submodule?
 cd graphhopper-maps
 npm install
+rm dist/bundle*js
 npm run build
+# we have a unique ID attached to the bundle file due to caching; remove this here
+mv dist/bundle.*.js dist/bundle.js
 cd ..
 
 npm install
 
-# Create Dist folder
+# Create dist folder
 npm run build
+# config.js is no longer in bundle.js
+cp config.js dist/
 
 # Create launch and splash icons
-npx cordova-res android --skip-config --copy
+npx capacitor-assets generate --android --iconBackgroundColor '#eeeeee' --iconBackgroundColorDark '#222222' --splashBackgroundColor '#ffffff' --splashBackgroundColorDark '#ffffff'
 
 npx cap sync
 
--- config.js
+++ config.js
@@ -1,18 +1,34 @@
-/**
- * This file will be copied to the GraphHopper Maps folder, please change this file and not the on in the GraphHopper Maps Submodule
- */
-module.exports = {
+const config = {
     // the url of the GraphHopper backend, either use graphhopper.com or point it to your own GH instance
     api: 'https://graphhopper.com/api/1/',
     // the tile layer used by default, see MapOptionsStore.ts for all options
-    defaultTiles: 'MapTiler',
+    routingApi: 'https://graphhopper.com/api/1/',
+    geocodingApi: 'https://graphhopper.com/api/1/',
+    defaultTiles: 'OpenStreetMap',
+    navigationTiles: 'MapTiler',
     // various api keys used for the GH backend and the different tile providers
     keys: {
-        graphhopper: "2b68713e-e80d-4f3d-8ddb-e3fcf72d8eac",
-        maptiler: "fDZ3K4ABPXKW2fdJrft5",
-        omniscale: "missing_api_key",
-        thunderforest: "missing_api_key",
-        kurviger: "missing_api_key"
-    }
+     "graphhopper":"2b68713e-e80d-4f3d-8ddb-e3fcf72d8eac",
+     "maptiler":"wYonyRi2hNgJVH2qgs81",
+     "omniscale":"_undefined_",
+     "thunderforest":"_undefined_",
+     "kurviger":"_undefined_"
+    },
+    // if true there will be an option to enable the GraphHopper routing graph and the urban density visualization in the layers menu
+    routingGraphLayerAllowed: false,
+    // parameters used for the routing request generation
+    request: {
+        details: [
+            'road_class',
+            'road_environment',
+            'surface',
+            'max_speed',
+            'average_speed',
+            'toll',
+            'track_type',
+            'country',
+        ],
+        snapPreventions: ['ferry'],
+    },
+    profiles: { car:{}, small_truck:{}, truck:{}, scooter:{}, foot:{}, hike:{}, bike:{}, mtb:{}, racingbike:{} },
 }
-
