1bcb99c301b1cfc97925d644eab92dd0ed009036
Peter <graphhopper@gmx.de>
2025-05-02 21:04:47 +0200

    update dependencies

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ const config = {
     navigationTiles: 'Mapilion',
     // various api keys used for the GH backend and the different tile providers
     keys: {
-     "graphhopper":"2b68713e-e80d-4f3d-8ddb-e3fcf72d8eac",
+     "graphhopper":"e96f6517-5a46-4e3d-9773-4956d7b6046f",
      "maptiler":"_undefined_",
      "omniscale":"_undefined_",
-     "thunderforest":"95b7c76e19c04e36ab9756f2cdf15b32",
+     "thunderforest":"4f283c77a8644d0b8ead1a3c3faf04ba",
      "kurviger":"b582abd4-d55d-4cb1-8f34-f4254cd52aa7"
     },
     // if true there will be an option to enable the GraphHopper routing graph and the urban density visualization in the layers menu
@@ -31,9 +31,43 @@ const config = {
         ],
         snapPreventions: ['ferry'],
     },
+    profile_group_mapping: {
+      car: {
+        options: [
+          { profile: 'car' },
+          { profile: 'car_avoid_motorway' },
+          { profile: 'car_avoid_ferry' },
+          { profile: 'car_avoid_toll' },
+        ]
+      },
+      racingbike: {
+        options: [
+          { profile: 'racingbike' },
+          { profile: 'ecargobike' } 
+        ]
+      },
+      truck: {
+        options: [
+          { profile: 'small_truck' },
+          { profile: 'truck' },
+        ]
+      },
+      foot: {
+        options: [
+          { profile: 'foot' },
+          { profile: 'hike' },
+        ]
+      },
+    },
     profiles: {
-      car:{}, small_truck:{}, truck:{}, scooter:{},
-      foot:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating'] }, hike:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating' ] },
-      bike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating' ] }, mtb:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] }, racingbike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] },
+      car:{}, car_avoid_motorway:{}, car_avoid_toll:{}, car_avoid_ferry:{},
+      small_truck:{}, truck:{},
+      scooter:{},
+      foot:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating'] },
+      hike:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating' ] },
+      bike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating' ] },
+      mtb:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] },
+      racingbike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] },
+      ecargobike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] },
     },
 }
