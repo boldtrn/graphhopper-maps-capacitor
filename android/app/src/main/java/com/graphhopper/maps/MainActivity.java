beaa53f690851e2da3c4e5b2eb68cfb8f20d2d73
Peter <graphhopper@gmx.de>
2026-01-22 16:00:56 +0100

    status bar issue fixed (notch)

--- android/app/src/main/java/com/graphhopper/maps/MainActivity.java
+++ android/app/src/main/java/com/graphhopper/maps/MainActivity.java
@@ -1,6 +1,42 @@
 package com.graphhopper.maps;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        applySystemBarInsets();
+        // Set background color for area behind status bar, with dark icons
+        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        controller.setAppearanceLightStatusBars(true);
+    }
+
+    // Workaround for Capacitor 8 edge-to-edge enforcement
+    // See: https://github.com/ionic-team/capacitor/issues/7951
+    private void applySystemBarInsets() {
+        View webView = getBridge().getWebView();
+        ViewCompat.setOnApplyWindowInsetsListener(webView, (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(
+                WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout()
+            );
+            ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            params.topMargin = insets.top;
+            params.bottomMargin = insets.bottom;
+            params.leftMargin = insets.left;
+            params.rightMargin = insets.right;
+            v.setLayoutParams(params);
+            return WindowInsetsCompat.CONSUMED;
+        });
+    }
 }
